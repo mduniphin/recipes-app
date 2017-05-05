9c19f73f3de0f7d2ff1ebe219c25b5e75b72acf9
mduniphin <maxx.duniphin@gmail.com>
2017-05-04 22:52:41 -0500

    created basic recipe model

---
+++ models/index.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+	title: String,
+	author: String,
+	ingredients: String,
+	instructions: String, 
+	date: Date,
+	is_active: Schema.Types.Boolean
+});
+
+module.exports = mongoose.model('auth', schema);
